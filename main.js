874a22ee9cd4d7c54dfdd893442a2c176941463b
Nacho <ta.descalzo@gmail.com>
2021-08-01 17:48:37 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAobg1h5aqprSpyT9T5XMtm9Z2H69z8T64",
+  authDomain: "transito-makro.firebaseapp.com",
+  projectId: "transito-makro",
+  storageBucket: "transito-makro.appspot.com",
+  messagingSenderId: "552286622434",
+  appId: "1:552286622434:web:a49a53fe669a907c91c4bb",
+  measurementId: "G-BXB2FGR5MV",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+verificar = () => {
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+};
