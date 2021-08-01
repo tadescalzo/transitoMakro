02a8e4ca6b2cab10fed77e7b8c06529f9922e281
Nacho <ta.descalzo@gmail.com>
2021-08-01 20:18:25 -0300

    function user login

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const firebaseConfig = {
+let logBtn = document.querySelector("#logBtn");
+let usrInput = document.querySelector("#usrInput");
+let pswInput = document.querySelector("#pswInput");
+var firebaseConfig = {
   apiKey: "AIzaSyAobg1h5aqprSpyT9T5XMtm9Z2H69z8T64",
   authDomain: "transito-makro.firebaseapp.com",
   projectId: "transito-makro",
@@ -8,32 +11,58 @@ const firebaseConfig = {
   measurementId: "G-BXB2FGR5MV",
 };
 firebase.initializeApp(firebaseConfig);
+firebase.auth();
 
-let googleBtn = document.querySelector("#googleBtn");
-
-function googleSingin() {
+/* function googleSingin() {
   var provider = new firebase.auth.GoogleAuthProvider();
   firebase
     .auth()
     .signInWithPopup(provider)
-    .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // The signed-in user info.
+    .then(function (result) {
+      // code which runs on success
       var user = result.user;
+      console.log(user.displayName, user.email);
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      console.log(errorCode);
+      alert(errorCode);
+
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      alert(errorMessage);
+    });
+}
+
+function googleSingout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      console.log("saliste");
+    })
+    .catch((error) => {
+      console.log("no habia cuenta");
+    });
+} */
+logFunct = (email, password) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+      console.log(user);
       // ...
     })
     .catch((error) => {
-      // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
     });
-}
+};
+
+logBtn.addEventListener("click", () => {
+  let password = pswInput.value;
+  let email = usrInput.value;
+  logFunct(email, password);
+});
