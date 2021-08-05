a0efd8235f695bb5e56423af745814324fe29cbd
Nacho <ta.descalzo@gmail.com>
2021-08-05 00:43:13 -0300

    sistema de objetos en backend

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ let usrInput = document.querySelector("#usrInput");
 let pswInput = document.querySelector("#pswInput");
 let loginSection = document.querySelector("#loginSection");
 let navName = document.querySelector(".navName");
+let linkAdd = document.querySelector(".linkAdd");
+let modalSection = document.querySelector("#modalSection");
+let modalSubmitBtn = document.querySelector(".modalSubmitBtn");
+let modalCloseBtn = document.querySelector(".modalCloseBtn");
+let modalTitle = document.querySelector("#modalTitle");
+let modalTkt = document.querySelector("#modalTkt");
+let modalDesc = document.querySelector("#modalDesc");
+let modalType = document.querySelector("#modalType");
+var modalOptions = document.querySelector("#options");
+let itemList = new Array();
 var firebaseConfig = {
   apiKey: "AIzaSyAobg1h5aqprSpyT9T5XMtm9Z2H69z8T64",
   authDomain: "transito-makro.firebaseapp.com",
@@ -14,6 +24,7 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 firebase.auth();
+var db = firebase.firestore();
 
 logFunct = (email, password) => {
   firebase
@@ -51,4 +62,54 @@ firebase.auth().onAuthStateChanged((user) => {
     navName.innerHTML = `Bienvenido ${user.displayName}`;
   } else {
   }
+});
+
+linkAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalSection.style.display = "flex";
+});
+
+modalCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalSection.style.display = "none";
+  modalTitle.value = "";
+  modalTkt.value = "";
+  modalDesc.value = "";
+  modalType.value = "";
+});
+
+modalSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  title = modalTitle.value;
+  tkt = modalTkt.value;
+  desc = modalDesc.value;
+  var valor = modalOptions.options[modalOptions.selectedIndex].value;
+  console.log(valor);
+
+  db.collection("itemsTransito")
+    .add({
+      title: this.title,
+      tkt: this.tkt,
+      desc: this.desc,
+    })
+    .then((docRef) => {
+      db.collection("itemsTransito")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            let indItem = doc.data();
+            console.log(
+              `${indItem.desc} => ${indItem.title} => ${indItem.tkt}`
+            );
+          });
+        });
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+
+  modalSection.style.display = "none";
+  modalTitle.value = "";
+  modalTkt.value = "";
+  modalDesc.value = "";
 });
